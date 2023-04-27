496446a1c0c90d681856506782dc7246a7bf1a3f
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-04-27 16:53:51 +0300

    excel formatting, better data gathering

---
+++ keywords.py
@@ -0,0 +1,7 @@
+# keywords.py
+building_keywords = ["RAKENNUS" "RAKENNUS A", "RAKENNUS B", "RAKENNUS C",
+                     "RAKENNUS D", "RAKENNUS E", "TALO", "TALO A", "TALO B", "TALO C", "TALO D", "TALO E"]
+floor_keywords = ["KERROS", "KERROKSET", "KRS", "VESIKATTO", "KELLARI"]
+apartment_keywords = ["AS \d+", "A \d+", "B \d+", "C \d+"]
+room_keywords = ["ET", "AH", "KT", "MH", "OH", "KPH", "WC", "eteinen", "aula", "keittiö",
+                 "makuuhuone", "olohuone", "kylpyhuone", "wc", "kodinhoitohuone", "vaatehuone", "terassi", "parveke"]
